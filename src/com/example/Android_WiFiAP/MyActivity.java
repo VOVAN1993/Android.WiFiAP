5f412f3fef2016b9ed36b36f651e3f87fdfa59e9
VOVAN1993 <cska631@gmail.com>
2014-01-03 18:19:34 +0400

    Created access point. Server is running. He sends them a received message

--- src/com/example/Android_WiFiAP/MyActivity.java
+++ src/com/example/Android_WiFiAP/MyActivity.java
@@ -1,9 +1,19 @@
 package com.example.Android_WiFiAP;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import java.net.SocketException;
+import java.util.ArrayList;
+
+//TODO : сделать отдельный класс сервер клиент
 public class MyActivity extends Activity {
+    TextView textView1;
+    MyWiFIAPManager wifiManager;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +21,34 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        textView1 = (TextView) findViewById(R.id.n);
+        boolean is_server = true;
+        wifiManager = null;
+        if (is_server) {
+            wifiManager = new MyWiFIAPManager((WifiManager) getSystemService(Context.WIFI_SERVICE), "vova");
+            try {
+                wifiManager.createWifiAccessPoint();
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+        }
+
+        wifiManager.start_client_handler();
+
     }
+
+    private void scan() {
+        ArrayList<ClientScanResult> clients = Util.getClientList();
+
+        textView1.append("Clients: \n");
+        textView1.append(String.valueOf(clients.size()));
+        for (ClientScanResult clientScanResult : clients) {
+            textView1.append("####################\n");
+            textView1.append("IpAddr: " + clientScanResult.getIpAddr() + "\n");
+            textView1.append("Device: " + clientScanResult.getDevice() + "\n");
+            textView1.append("HWAddr: " + clientScanResult.getHWAddr() + "\n");
+        }
+    }
+
 }
